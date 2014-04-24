4e30cc16c08d760928449cc22d276b9dfacb27b5
Jon Snider <jon@jon.local>
2014-04-24 18:10:45 -0400

    Initial Commit

---
+++ HelloWorld/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Jon Snider on 4/24/14.
+//  Copyright (c) 2014 orpheus io. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
